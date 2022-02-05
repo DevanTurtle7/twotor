1b3882b15703f5538b8c91c9ec176c2550c8374a
Ethan Iannicelli <etiannicelli@gmail.com>
2022-02-05 15:39:29 -0500

    added to server

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from auth import Login, CreateAccount, Logout
-from welcome_api import ListSubjects, ListCourses, ListUniversities
+from welcome_api import ListSubjects, ListCourses, ListUniversities, ListQueue
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,6 +24,7 @@ api.add_resource(CreateAccount, '/signup')
 api.add_resource(ListSubjects, '/subjects/<int:university_id>')
 api.add_resource(ListCourses, '/courses/<int:subject_id>')
 api.add_resource(ListUniversities, '/universities')
+api.add_resource(ListQueue, '/queue/<int:course_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
