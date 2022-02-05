672bc8b2fde23008d29925edb9aace7583fe6889
Nick Dawson <nick@ndawson.me>
2022-02-05 13:43:20 -0500

    Login, logout, create account working

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-from auth import Login, CreateAccount
+from auth import Login, CreateAccount, Logout
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +16,7 @@ api.add_resource(Test, '/')
 
 # Auth
 api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 api.add_resource(CreateAccount, '/signup')
 
 if __name__ == '__main__':
