a606b76d4ecdb7836ee62296b6b92a402712d999
Devan Kavalchek <devankav7@gmail.com>
2022-02-05 11:39:55 -0500

    added form completion

--- frontend/src/components/SchoolForm.jsx
+++ frontend/src/components/SchoolForm.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import {
+    Dropdown,
+    DropdownMenu,
+    DropdownToggle,
+    DropdownItem
+} from "reactstrap";
 import FormWrapper from "./FormWrapper";
 
 function SchoolForm(props) {
+    const onChange = () => {
+        const index = props.index
+        props.setComplete(index, true);
+    }
+
     return (
         <FormWrapper index={props.index} current={props.current}>
-            <h1>This is my form weeeeeee</h1>
+            <h2>Select your university</h2>
+
+            <select onChange={onChange}>
+                <option value="" disabled selected>Select your university...</option>
+                <option>Rochester Institute of Technology</option>
+            </select>
         </FormWrapper>
     )
 }
--- frontend/src/pages/SetupPage.jsx
+++ frontend/src/pages/SetupPage.jsx
@@ -1,36 +1,54 @@
 import '../style/SetupPage.css';
 import SchoolForm from '../components/SchoolForm';
 import SubjectForm from '../components/SubjectForm';
-import {Button} from 'reactstrap';
+import { Button } from 'reactstrap';
 import { useState } from 'react';
 
-const MAX_INDEX = 2;
+const MAX_INDEX = 1;
 
 function SetupPage(props) {
     const [index, setIndex] = useState(0);
+    const [completedForms, setCompletedForms] = useState({
+        0: false,
+        1: false
+    })
+
+    const canGoNext = () => index < MAX_INDEX && currentCompleted()
+    const canGoPrev = () => index > 0
 
     const next = () => {
-        if (index < MAX_INDEX) {
+        if (canGoNext()) {
             setIndex(index + 1)
         }
     }
 
     const prev = () => {
-        if (index > 0) {
+        if (canGoPrev()) {
             setIndex(index - 1)
         }
+    }
+
+    const currentCompleted = () => {
+        return completedForms[index]
+    }
+
+    const setComplete = (index, complete) => {
+        setCompletedForms(completedForms => ({
+            ...completedForms,
+            [index]: complete
+        }))
     }
 
     return (
         <div className='page-col' id='setup-page'>
             <div className='form-container'>
-                <SchoolForm index={0} current={index} />
-                <SubjectForm index={1} current={index} />
+                <SchoolForm index={0} current={index} setComplete={setComplete}/>
+                <SubjectForm index={1} current={index} setComplete={setComplete} />
             </div>
 
             <div className='form-footer'>
-                <Button onClick={prev}>Back</Button>
-                <Button onClick={next}>Next</Button>
+                <Button onClick={prev} disabled={!canGoPrev()}>Back</Button>
+                <Button onClick={next} color='primary' disabled={!canGoNext()}>Next</Button>
             </div>
         </div>
     )
