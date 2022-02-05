677681fb767fecb8d2cb0c02822d1e201cd1f97c
Ethan Iannicelli <etiannicelli@gmail.com>
2022-02-05 15:12:41 -0500

    listunis

--- backend/welcome_api.py
+++ backend/welcome_api.py
@@ -1,7 +1,12 @@
 from flask_restful import Resource
 from flask import request, make_response
 
+from database import db
+
 class ListUniversities(Resource):
     def get(self):
-        # Ethan do this
-        pass
+        sql = """
+        SELECT id, name
+        FROM universities
+        """
+        return db.exec_get_all_json(sql)
