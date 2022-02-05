37f160fae34413a919cbb0aaad8db1f426fb90ae
Ethan Iannicelli <etiannicelli@gmail.com>
2022-02-05 16:59:56 -0500

    added update to server

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ from flask_cors import CORS, cross_origin
 from auth import Login, CreateAccount, Logout
 from welcome_api import ListSubjects, ListCourses, ListUniversities
 from queue import ListQueue
+from update_course import *
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -28,7 +29,18 @@ api.add_resource(CreateAccount, '/signup')
 api.add_resource(ListSubjects, '/subjects/<int:university_id>')
 api.add_resource(ListCourses, '/courses/<int:subject_id>')
 api.add_resource(ListUniversities, '/universities')
+
+# Queue
 api.add_resource(ListQueue, '/queue/<int:course_id>')
+
+# Update
+api.add_resource(AddHelpCourse, '/addhelp')
+api.add_resource(AddTutorCourse, '/addtutor')
+api.add_resource(DelHelpCourse, '/delhelp')
+api.add_resource(DelTutorCourse, '/deltutor ')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
