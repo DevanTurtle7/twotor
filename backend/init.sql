e8420c518faa81cbfcb98880311572131bcfb7a5
Nick Dawson <nick@ndawson.me>
2022-02-05 13:54:52 -0500

    Changing database

--- backend/init.sql
+++ backend/init.sql
@@ -1,12 +1,21 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS universities, accounts;
+
+CREATE TABLE universities (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+INSERT INTO universities (name) VALUES
+('Rochester Institute of Technology');
 
 CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
     email TEXT UNIQUE NOT NULL,
+    university_id INTEGER REFERENCES universities ON DELETE SET NULL,
     username VARCHAR(16) UNIQUE NOT NULL,
     salt VARCHAR(128) NOT NULL,
     password VARCHAR(128) NOT NULL, -- sha512 produces 128bit hex number
