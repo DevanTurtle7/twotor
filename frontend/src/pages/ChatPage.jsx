5885f93f5838bf02fc1944165ba7e49d79901f92
Devan Kavalchek <devankav7@gmail.com>
2022-02-05 19:56:31 -0500

    finished chat page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,9 @@
 import '../style/ChatPage.css'
 import { Input } from 'reactstrap';
-import { MdSend } from "react-icons/md";
+import { MdSend, MdArrowBack } from "react-icons/md";
 import ChatBubble from '../components/ChatBubble'
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ChatPage(props) {
     const messages = [
@@ -48,25 +49,26 @@ function ChatPage(props) {
         {
             sender: 0,
             message: "ashjdkaj kdjaklsjdkl jaslkdj lkasjlkd jaklsdjlkasjlk djalkj sdlkaslkjdlkaj sldk "
-        },        {
+        }, {
             sender: 0,
             message: "asdhj jkasjlkasdlkjas dlalklkl lj ljfd ljd jldjlkdjldfklj gdljflkjdflk dlkj ldkdlk j"
-        },        {
+        }, {
             sender: 0,
             message: "asldjias jdlalid jlaksj ldjalkj dlkjaslk d"
-        } ,       {
+        }, {
             sender: 0,
             message: "ashjdkaj kdjaklsjdkl jaslkdj lkasjlkd jaklsdjlkasjlk djalkj sdlkaslkjdlkaj sldk "
-        },        {
+        }, {
             sender: 0,
             message: "asdhj jkasjlkasdlkjas dlalklkl lj ljfd ljd jldjlkdjldfklj gdljflkjdflk dlkj ldkdlk j"
-        },        {
+        }, {
             sender: 0,
             message: "asldjias jdlalid jlaksj ldjalkj dlkjaslk d"
         },
     ]
 
     const [text, setText] = useState("")
+    const navigate = useNavigate();
 
     const createChatBubbles = () => {
         const bubbles = []
@@ -102,14 +104,23 @@ function ChatPage(props) {
 
     }
 
+    const goBack = () => {
+        navigate("/home")
+    }
+
     return (
         <div id="chat-page">
+            <div id="navbar">
+                <h3 className='message-label'>
+                    <MdArrowBack className='back-arrow clickable' onClick={goBack} /> Nick Dawson
+                </h3>
+            </div>
             <div id="chat-history">
                 {createChatBubbles()}
             </div>
             <div id="message-bar">
                 <div id="input-container">
-                    <Input placeholder='Send Message' type='text' id='message-input' onChange={textChanged}/>
+                    <Input placeholder='Send Message' type='text' id='message-input' onChange={textChanged} />
                 </div>
                 <div id="button-container">
                     <button className='circle-button' onClick={sendMessage}>
